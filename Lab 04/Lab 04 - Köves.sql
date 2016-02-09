997302ee0193299d9b450198a5bbc14ce812f0d7
Krisztián Köves <kkoves3@gmail.com>
2016-02-09 12:14:14 -0500

    Started Lab 4; wrote first query, updated PDF

---
+++ Lab 04/Lab 04 - Köves.sql
@@ -0,0 +1,15 @@
+﻿---------------------------------------------------------------------
+--   Title: Lab 4 SQL Script                                        |
+-- Purpose: Makes 8 queries according to the description for Lab 4. |
+--  Author: Krisztián Köves                                         |
+--    Date: 2/9/2016                                                |
+---------------------------------------------------------------------
+
+-- Query 1: "Get the cities of agents booking an order for a customer whose cid is 'c002'."
+SELECT city
+FROM agents
+WHERE aid IN (SELECT aid
+              FROM orders
+              WHERE cid = 'c002'
+             );
+
